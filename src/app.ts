b40c06b67ba33455e6263284b038cfa27ec40aee
Maria Eduarda <maria.freitas.eduarda694@gmail.com>
2024-10-08 13:57:52 -0300

    Elitecar-Backend

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,5 @@ import {server} from './server';
 const port: number = 3333;
 
 server.listen(port, () => {
-    console.log('Endereço do servidor: http://localhost:${port}')
+    console.log('Endereço do servidor: http://localhost:${3333}')
 });
